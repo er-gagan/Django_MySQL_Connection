2ef6a49e462487515f1d1cf0ee4f6b2556313cd6
Gagan Aggarwal <62302397+pythoniseasy-hub@users.noreply.github.com>
2020-12-07 21:10:53 +0530

    Project Structure create
    Project Structure create
    frontend backend complete
    models create

---
+++ DjMySQLENV/DjMySQL/MyApp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+from .models import Student
+from django.http.response import HttpResponse
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+@csrf_exempt
+def submit(request):
+    if request.method == "POST":
+        Name = request.POST['Name']
+        Age = request.POST['Age']
+        Phone = request.POST['Phone']
+        Student(Name = Name, Age = Age, Phone = Phone).save()
+        msg = "Record Stored into Database"
+        return render(request,"home.html",{'msg':msg})
+    else:
+        return HttpResponse("404 - Not Found")
